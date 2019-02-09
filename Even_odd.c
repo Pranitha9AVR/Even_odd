98ba4dd8ef7179f059bc935f09a331c4364a8ada
Pranitha9AVR <47452339+Pranitha9AVR@users.noreply.github.com>
2019-02-09 16:22:40 +0530

    Create Even_odd.c

---
+++ Even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+   {
+    printf("Even");
+    } 
+   else if(a%2>0)
+   {
+   printf("Odd"); 
+   }
+  else
+  {
+    printf("invalid");
+  }
+return 0;
+}
